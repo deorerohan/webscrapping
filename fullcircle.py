7a584f1a2d3e474a031a177ea959779c799a109a
Rohan Deore <deorerohan@gmail.com>
2015-06-07 03:47:32 +0530

    adding place holder files for webscrapping for fullcircle and greys

---
+++ fullcircle.py
@@ -0,0 +1,4 @@
+
+
+# link http://fullcirclemagazine.org/issue-48/
+# name : _en.epub
---
+++ greys.py
@@ -0,0 +1,8 @@
+# link : http://www.peppertop.com/greys/comic/yeti-boots/
+# This is first comic and there after we need to download
+# there should be some check to avoid loading pages for which downloads
+# are completed
+# All scrapping projects should be kept in single file 
+# but divided into different classes 
+# so that common codes and functionalities such as check for previous downloads
+# can go in different classed
